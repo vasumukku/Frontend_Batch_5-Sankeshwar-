ae2d1e52f7d1e5bb2c109c82ec48f3595bb42614
vasumukku <mukkuvasu01@gmail.com>
2026-04-24 16:16:47 +0530

    day-5 added

---
+++ Day4/dom.js
@@ -0,0 +1,4 @@
+let a = document.getElementById("1");
+function changetext() {
+  a.textContent="hello summith babai..."
+} 
---
+++ Day4/first.js
@@ -0,0 +1,13 @@
+
+for(let i=1;i<=20;i++){
+  let even_count=0;
+  let odd_count=0;
+  if(i%2==0){
+    even_count=even_count+1;
+  }else{
+    odd_count=odd_count+1
+  }
+  console.log("even count is : "+ even_count);
+console.log("odd count is : "+ odd_count);
+}
+
