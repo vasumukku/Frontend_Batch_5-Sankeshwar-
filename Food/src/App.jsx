fe91c272c3a9ed0397dd2309c836299f061ba8dc
vasumukku <mukkuvasu01@gmail.com>
2026-04-30 14:09:56 +0530

    final project

--- Food/src/App.jsx
+++ Food/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div>
       <div className='cards'>
         <Body  img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuaGZZh_vkBocGSLUDExvKyspUrmuryzz92g&s" name="dobberman" price="₹29,999"/>
-      <Body  img="https://images.aeonmedia.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/essay-final-gettyimages-685469924.jpg?width=3840&quality=75&format=auto"  name="husky"/>
+      <Body  img="https://images.aeonmedia.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/essay-final-gettyimages-685469924.jpg?width=3840&quality=75&format=auto"  name="husky" price="19.99"/>
        <Body  img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuaGZZh_vkBocGSLUDExvKyspUrmuryzz92g&s"/>
       <Body  img="https://images.aeonmedia.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/essay-final-gettyimages-685469924.jpg?width=3840&quality=75&format=auto"/>
 
